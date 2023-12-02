85fc354b958f3937472a45a030aab1cce8f9cf98
Morphius22 <86893963+Morphius22@users.noreply.github.com>
2023-12-02 01:02:54 +0000

    added logic for clicks removing pictiures

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,8 +1,15 @@
 import { useEffect, useState } from "react"
 
-export default function Card({character}) {
+export default function Card({character, characterList, setCharacterList}) {
+
+  const handleCardClick = (characterId, character) => {
+    const newCharacterList = [...characterList];
+    const filteredCharacterList = newCharacterList.filter(character => characterId !== character.id);
+    setCharacterList(filteredCharacterList);
+  };
+
   return (
-    <div className="card">
+    <div className="card" onClick={() => handleCardClick(character.id, character)}>
           <img src={character.image} alt={character?.name}></img>
           <p>{character.name}</p>
     </div>
--- src/GameBoard.jsx
+++ src/GameBoard.jsx
@@ -2,24 +2,22 @@ import { useState, useEffect } from "react";
 import Card from "./Card";
 
 export default function GameBoard() {
-  const [characters, setCharacters] = useState([]);
+  const [characterList, setCharacterList] = useState([]);
   const [gameWon, setGameWon] = useState();
-  const [characterCount, setCharacterCount] = useState();
 
   useEffect(() => {
     fetch("https://rickandmortyapi.com/api/character/?page=2")
       .then(res => res.json())
       .then(data => {
         console.log(`return from api fetch: ${data.results}`)
-        setCharacters(data.results)
+        setCharacterList(data.results)
       })
   },[])
   
   return(
     <div className="gameBoard">
-      
-      {characters.length > 0 && (characters.map(character => {
-        return <Card key={character.id} character={character}/>
+      {characterList.length > 0 && (characterList.map(character => {
+        return <Card key={character.id} character={character} characterList={characterList} setCharacterList={setCharacterList}/>
       }))}
     </div>
   )
